718ff856c8deb3ae9582fcbb5fa8f5a6260484d1
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-08-20 02:31:30 -0300

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -358,6 +358,19 @@ class Alphabet extends FlxSpriteGroup
 			}
 		}
 
+		if (isMenuItemCenter)
+			{
+				var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
+	
+				var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
+				y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
+				if(forceX != Math.NEGATIVE_INFINITY) {
+					screenCenter(X);
+				} else {
+					screenCenter(X);
+				}
+			}
+
 		super.update(elapsed);
 	}
 
